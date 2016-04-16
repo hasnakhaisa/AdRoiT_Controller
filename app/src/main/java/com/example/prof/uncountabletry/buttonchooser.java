df7fc4859bcff642559f842589646aa3b0e089d8
tegarimansyah <egar.imansyah@gmail.com>
2016-04-16 10:05:39 +0700

    First Commit, alpha version

---
+++ app/src/main/java/com/example/prof/uncountabletry/buttonchooser.java
@@ -0,0 +1,50 @@
+package com.example.prof.uncountabletry;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class buttonchooser extends AppCompatActivity {
+
+    String[] Abutton = {"button_up","button_down","button_right","button_left","button_A","button_B","button_C","button_D","button_E"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buttonchooser);
+
+        ArrayAdapter adapter  = new ArrayAdapter<String>(this, R.layout.activity_buttonlist,Abutton);
+
+        ListView listView = (ListView) findViewById(R.id.button_list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new ListClickHandler());
+    }
+
+    public class ListClickHandler implements OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> adapter, View view, int position, long arg3) {
+            // TODO Auto-generated method stub
+            String msg = "Android : ";
+            TextView listText = (TextView) view.findViewById(R.id.label);
+            String text = listText.getText().toString();
+            //Log.d(msg, text);
+            Intent intent=new Intent();
+            intent.putExtra("MESSAGE",text);
+            setResult(2,intent);
+            finish();//finishing activity
+
+        }
+
+    }
+
+
+}
